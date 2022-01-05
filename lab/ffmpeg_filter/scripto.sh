01a050e909ee62936072bd7dba677336dcfcfc5a
lucasew <lucas59356@gmail.com>
2022-01-05 14:59:59 -0300

    some stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lab/ffmpeg_filter/scripto.sh
+++ lab/ffmpeg_filter/scripto.sh
@@ -2,8 +2,9 @@ rm out.mp4 || true > /dev/null
 export FILTER='
 [0:v]scale=1920*2:-1,crop=1920:1080,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];
 [0:v]scale=-1:1080[ov];
-[bg][ov]overlay=(W-w)/2,crop=1920:1080
+[bg][ov]overlay=(W-w)/2,crop=1920:1080[composed];
+anullsrc=cl=stereo:r=44100[anull]
 '
 
 echo $FILTER
-ffmpeg -y -i src.mp4 -filter_complex "$FILTER" -c:a copy out.mp4 && vlc out.mp4 --fullscreen --play-and-exit
+ffmpeg -y  -i src.mp4 -t 15 -filter_complex "$FILTER" -map '[composed]' -map '0:a?' -map '[anull]' -shortest out.mkv && vlc out.mkv --fullscreen --play-and-exit
